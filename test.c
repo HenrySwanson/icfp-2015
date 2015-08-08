22ea445d7dd88932ab41f8d8bab7a61e68bc6f86
Henry Swanson <henryswanson94@gmail.com>
2015-08-08 10:30:39 -0500

    Add weak-sauce tests
    Better than nothing...

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "board.h"
@@ -35,20 +36,58 @@ void print_board(const board b)
     }
 }
 
-int main()
+void test_shifts()
+{
+    int x = 2;
+    int y = 1;
+
+    shift_cell(&x, &y, EAST);
+    assert(x == 3 && y == 1);
+
+    shift_cell(&x, &y, SOUTHEAST);
+    assert(x == 4 && y == 2);
+
+    shift_cell(&x, &y, SOUTHEAST);
+    assert(x == 4 && y == 3);
+
+    shift_cell(&x, &y, WEST);
+    assert(x == 3 && y == 3);
+
+    shift_cell(&x, &y, SOUTHWEST);
+    assert(x == 3 && y == 4);
+
+    shift_cell(&x, &y, SOUTHWEST);
+    assert(x == 2 && y == 5);
+}
+
+void test_clear_rows()
 {
     board b;
+    alloc_board(&b, 3, 5);
 
-    int array [15] = { 1, 1, 1,
-                       0, 1, 1,
-                       1, 1, 1,
-                       0, 0, 0,
-                       0, 0, 1 };
-    b.width = 3;
-    b.height = 5;
-    b.cells = array;
+    int start [3][5] = {{1, 0, 1, 0, 0},
+                        {1, 1, 1, 0, 0},
+                        {1, 1, 1, 0, 1}};
 
-    print_board(b);
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 5; j++)
+            set_cell(&b, i, j, start[i][j]);
+
     clear_rows(&b);
-    print_board(b);
+
+    int end [3][5] = {{0, 0, 0, 0, 0},
+                      {1, 0, 0, 0, 0},
+                      {1, 0, 1, 0, 0}};
+
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 5; j++)
+            assert(get_cell(b, i, j) == end[i][j]);
+
+    free_board(b);
+}
+
+int main()
+{
+    test_shifts();
+    test_clear_rows();
 }
